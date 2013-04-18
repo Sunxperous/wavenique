0bf7cf37b653bd4240732a4257993d6e7033cfdf
Sunxperous <sunxperous@gmail.com>
2013-04-18 13:29:50 +0000

    Initial commit after Google API module.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vagrant::Application.config.secret_token = '8946fda380b76ce7fb668d5d0fbbe279ae507f4fe753e0da934182802ee80a6f796c3716ce3627b7c87af903457a51d88723950b0846f28d1ce8fefa6afc6f5d'
